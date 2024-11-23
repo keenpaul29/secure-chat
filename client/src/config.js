6be094c4aef5981a6c0178ce6f8533e504665c7b
Puspal <97587474+keenpaul29@users.noreply.github.com>
2024-11-23 18:57:10 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,9 +1,9 @@
 const config = {
   SERVER_URL: process.env.NODE_ENV === 'production' 
-    ? 'https://secure-chat-app-server.onrender.com' // Replace with your Render.com URL after deployment
+    ? 'https://secure-chat-server.onrender.com/' // Replace with your Render.com URL after deployment
     : 'http://localhost:5001',
   SOCKET_URL: process.env.NODE_ENV === 'production'
-    ? 'https://secure-chat-app-server.onrender.com' // Replace with your Render.com URL after deployment
+    ? 'https://secure-chat-server.onrender.com/' // Replace with your Render.com URL after deployment
     : 'http://localhost:5001'
 };
 
