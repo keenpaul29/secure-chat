5d068787b467a02f71f42778debe4c0adb74eec9
Puspal <97587474+keenpaul29@users.noreply.github.com>
2024-11-23 19:17:38 +0530

    Update production.js

--- server/config/production.js
+++ server/config/production.js
@@ -1,6 +1,6 @@
 module.exports = {
   cors: {
-    origin: 'https://secure-chat-h08ur4wea-keenpaul29s-projects.vercel.app', // For development, we'll accept all origins. In production, you should specify your Vercel URL
+    origin: 'https://secure-chat-client.vercel.app/', // For development, we'll accept all origins. In production, you should specify your Vercel URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
   }
